9a1de6e240071e55d9d9a5db4cc36ef8254c247f
charlesalmeiida <charlesalmeida844@gmail.com>
2025-02-12 18:52:23 -0300

    feat: integrando o sanity cms ao projeto

---
+++ src/sanity/schemaTypes/homepage.ts
@@ -0,0 +1,31 @@
+export const Homepage = {
+  name: "homepage",
+  title: "Homepage",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      description: "Título principal da Hero",
+    },
+    {
+      name: "subtitle",
+      title: "Subtitle",
+      type: "string",
+      description: "Subtitulo da Hero",
+    },
+    {
+      name: "form",
+      title: "Form",
+      type: "string",
+      description: "Texto principal do formulário",
+    },
+    {
+      name: "buttonCta",
+      title: "Button CTA",
+      type: "string",
+      description: "Texto do botão CTA",
+    },
+  ],
+}
